320bdc24b1998ab7652f4bfdd10cfe2285a7610e
JoaoJV90 <joao.jaramillo@gmail.com>
2025-06-10 21:01:08 -0500

    changes for music house

--- web_audioparts/settings.py
+++ web_audioparts/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-02#0en9ixm9$=_!#)0n^^x6$gpm*ljihq^jw8&3s$qt$=+41#8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
